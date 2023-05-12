b8658b7b8a1d21432dec6c3b359ea560c0c0c1f7
c4trisk <cathja.riska@gmail.com>
2023-05-12 11:52:45 +0200

    product schema

--- backend/schemas/productSchema.js
+++ backend/schemas/productSchema.js
@@ -5,6 +5,9 @@ const productSchema = mongoose.Schema({
   description:  { type: String, required: true },
   price:        { type: Number, required: true },
   imageURL:     { type: String, required: true },
+  tags:         { type: [String] },
+  rating:       { type: Number },
+  review:       { type: String }
 })
 
 module.exports = mongoose.model('Product', productSchema);
