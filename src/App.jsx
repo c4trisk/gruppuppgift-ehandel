8fc3d9b1f0a703672079726db378c397edefca0d
Izabelle Olofsson <izabelleolofsson@live.se>
2023-05-12 11:09:12 +0200

    smågrejs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home"
 import Products from "./pages/Products"
 import Contact from "./pages/Contact"
 import Login from "./pages/Login"
+import Registration from "./pages/Registration"
+import ProductDetails from "./pages/ProductDetails"
 
 const App = () => {
   return (
@@ -12,8 +14,10 @@ const App = () => {
         <Routes>
             <Route path="/" element= { <Home/>}/>
             <Route path="/products" element= { <Products/>}/>
+            <Route path="/products/:id" element= { <ProductDetails/>}/>
             <Route path="/contact" element= { <Contact/>}/>
             <Route path="/login" element= { <Login/>}/>
+            <Route path="/registration" element= { <Registration/>}/>
         </Routes>
     </div>
   )
