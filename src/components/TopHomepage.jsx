6e0ed40cd77bdd0117801ae366f8ae60746d1315
c4trisk <cathja.riska@gmail.com>
2023-05-17 15:52:18 +0200

    added components to Home

--- src/components/TopHomepage.jsx
+++ src/components/TopHomepage.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SizedProductCard from './SizedProductCard'
 import useFetch from './useFetch'
 
 const TopHomepage = () => {
 
   const { data: product, isLoading, error } = useFetch('http://localhost:9999/api/products')
+  const [firstProduct, setFirstProduct] = useState(0)
+  const [productCounter, setProductCounter] = useState(6)
+
+  const nextProducts = () => {
+
+    if(productCounter === 12) {
+      return
+    }
+
+    setFirstProduct(productCounter )
+    setProductCounter(productCounter * 2)
+
+  }
+
+  const prevProducts = () => {
+    if (firstProduct <= 5) {
+      return
+    }
+    setFirstProduct(firstProduct - 6)
+    setProductCounter(productCounter - 6)
+  }
 
   return (
     <div className='TopHomepage'>
         <h2 className='headline'>Top selling products in this week</h2>
       <div className="container">
-        {product && product.slice(0, 6).map((product) => (
+        {product && product.slice(firstProduct, productCounter).map((product) => (
               <SizedProductCard key={product._id} product={product} width={'270px'} height={'295px'} />
             ))}
+      </div>
+      <div className="buttons">
+        <button className="btn" onClick={() => prevProducts()}>&lt;</button>
+        <button className="btn" onClick={() => nextProducts()}>&gt;</button>
       </div>
     </div>
   )
