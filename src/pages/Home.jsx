313bc96a07e0f038143cff28193cb14a09a7aa8d
Frida Wieweg <fridaelisee@hotmail.com>
2023-05-17 13:24:12 +0200

    added new component in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import HeroHomepage from '../components/HeroHomepage'
 import Subscribe from '../components/Subscribe'
+import CollectionHomePage from '../components/CollectionHomePage'
 
 const Home = () => {
   return (
     <div>
       <HeroHomepage/>
+      <CollectionHomePage/>
       <Subscribe/>
+      
     </div>
   )
 }
