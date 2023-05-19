e16b2e649ced297465d18d8e9ddbdcd6a88430a3
Evelina Nyman <ina.nyman98@gmail.com>
2023-05-19 16:03:40 +0200

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import ContactForm from '../components/ContactForm'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+    <div className='contact-wrapper'>
+    <div className='contact-card'>
+    <p className='contact-headline'>Write something</p>
+      <ContactForm />
+    </div>
+    </div>
+    </div>
   )
 }
 
