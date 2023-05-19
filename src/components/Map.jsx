a23727b6de29ad3a4f0d4c0edeff98dc44b24dd4
Izabelle Olofsson <izabelleolofsson@live.se>
2023-05-19 14:30:02 +0200

    map

---
+++ src/components/Map.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+<div className='MapContainer'>
+     <iframe 
+     width="800" 
+     height="500" 
+     frameborder="0" 
+     src="https://www.bing.com/maps/embed?h=500&w=800&cp=59.07424273421398~11.245424597385636&lvl=11&typ=d&sty=r&src=SHELL&FORM=MBEDV8" 
+     scrolling="no">
+     </iframe>
+    
+     </div>
+  )
+}
+
+export default Map
